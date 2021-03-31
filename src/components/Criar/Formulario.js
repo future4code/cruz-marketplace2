e503083c8a1822c8e7f4e63b2afb9d263a487e1d
Fayra Miranda <miranda.fayra@gmail.com>
2021-03-30 21:57:38 -0300

    Criar lógica do input

--- src/components/Criar/Formulario.js
+++ src/components/Criar/Formulario.js
@@ -55,23 +55,48 @@ const Title = styled.h1 `
 
 
 
+
 // -------------------------------------------Structure---------------------------------------------
 export default class Formulario extends React.Component{
-    // state = {
-    //     inputTitulo="",
-    //     inputCategorias=[],
-    //     inputDescricao="",
-    //     inputValor="",
-    //     inputPrazo="",
-    // }; 
-    // 
+    state = {
+        inputTitulo: "", //Titulo do anúncio
+        inputCategorias=[], //Array de categorias de serviço
+        inputDescricao="", //Descriçao do serviço
+        inputValor="", //Valor a ser pago pelo serviço
+        inputPrazo="", //Prazo para execução do serviço - Formato de data
+    };
+    handleInputTitulo = (e) => {
+        this.setState({ inputTitulo: e.target.value });
+    };
+    handleInputCategorias = (e) => {
+        this.setState({ inputCategorias: e.target.value });
+    };
+    handleInputDescricao = (e) => {
+        this.setState({ inputDescricao: e.target.value });
+    };
+    handleInputValor = (e) => {
+        this.setState({ inputValor: Number(e.target.value) });
+    };
+    handleInputPrazo = (e) => {
+        this.setState({ inputPrazo: Number(e.target.value) });
+    };
+
+    //--------------------------------Função Cadastrar Novo Serviço-----------------------------------
+
+    cadastrarServico = () => {
+    const registrar = {
+        titulo: this.state.inputTitulo,
+        categorias: this.state.inputCategorias,
+        descricao: this.state.inputDescricao,
+        valor: this.state.inputValor,
+        prazo: this.state.inputPrazo,
+    };
+    }
+
+    // ---------------------------------------------Axios//API-----------------------------------------
     //
     //
-    //
-    //
-    //
-    //
-    
+
     render(){
         return(
             <BaseContainer>
@@ -82,36 +107,41 @@ export default class Formulario extends React.Component{
                     Título
                         <input type="text" name="tituloInput" 
                         placeholder="Insira o nome do seu serviço"
-                        />
+                        value={this.state.inputTitulo}
+                        onChange={this.handleInputTitulo}/>
                 </label>
                 <label>
                     Categorias
-                <select>
+                <select value={this.state.inputCategorias} onChange={this.handleInputCategorias}>
                     <option value="tecnica">Assistência Técnica</option>
                     <option value="aula">Aula</option>
-                    <option value="domestico">Serviços Domésticos</option>
-                    <option value="tecnologia">Design e Tecnologia </option>
                     <option value="consultoria">Consultoria</option>
-                    <option value="lifestyle">Lifestyle</option>
+                    <option value="tecnologia">Design e Tecnologia </option>
                     <option value="eventos">Eventos </option>
+                    <option value="lifestyle">Lifestyle</option>
                     <option value="obra">Obras e Reforma</option>
+                    <option value="domestico">Serviços Domésticos</option>
                 </select>
                 </label>
                 <label>
                     Descrição do Serviço
                         <input type="text" name="servicoInput" 
                         placeholder="Descrição do serviço"
-                        />
+                        value={this.state.inputDescricao}
+                        onChange={this.handleInputDescricao}/>
                 </label>
                 <label>
                     Valor do Serviço
                         <input type="number" name="valorInput" 
                         placeholder="Descrição do serviço"
-                        />
+                        value={this.state.inputValor}
+                        onChange={this.handleInputValor}/>
                 </label>
                 <label>
                     Prazo
-                    <input id="prazo" type="date"/>
+                    <input id="prazo" type="date"
+                    value={this.state.inputPrazo}
+                    onChange={this.handleInputPrazo}/>
                 </label>
                 <Cadastrar> Cadastrar </Cadastrar>
             </FormularioContainer>
