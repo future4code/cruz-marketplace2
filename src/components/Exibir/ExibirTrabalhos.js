1a562b974603b5aee982db89bad6f1e2e6f5aa4b
marcosmaiaideia <marcosmaia@gmail.com>
2021-04-01 21:39:42 -0300

    add Ordenação por valor

--- src/components/Exibir/ExibirTrabalhos.js
+++ src/components/Exibir/ExibirTrabalhos.js
@@ -7,14 +7,20 @@ import { api, img } from './constantes/API'
 export default class ExibirTrabalhos extends React.Component {
 
   state={
+    valorSelectOrdem: 'decrescente',
     jobs: []
   }
 
     //se tivesse inputs, funções de controle viriam aqui
 
-    componentDidMount() {
+  componentDidMount() {
       this.getApiData()
   }
+
+  onChangeSelectOrdem = (event) => {
+    this.setState( { valorSelectOrdem: event.target.value } )
+  }
+
 
   getApiData = () => {
     axios
@@ -43,27 +49,51 @@ export default class ExibirTrabalhos extends React.Component {
 
 
     //funções de filtragem para enviar ao JSX (tal como o map)
-    const jobsToScreen = this.state.jobs.map((job) => (
+    
+    const produtoFiltrado = this.state.jobs.filter ( job => {
+      
+      // //REGEX para o filtro por palavra com apenas algumas letras
+      // let regex = new RegExp('['+this.state.valorInputNome+']{4,}', "gmi")
+      // let resultadoBusca = regex.test(produto.produtoNome)
 
-      // <div className='list-item' key={job.id}>
+      // if ((
+      //   produto.produtoNome === this.state.valorInputNome || 
+      //   this.state.valorInputNome === '' ||
+      //   resultadoBusca
+      //   ) 
+      // && produto.produtoPreco >= this.state.valorInputMinimo && produto.produtoPreco <= this.state.valorInputMaximo ) {
+      //     return true
+      // }
+      // return false
+      return true 
+  })
+    //ORDENAÇÃO COM MÉTODO SORT
+    const produtoOrdenado = produtoFiltrado.sort( (a, b) => {
+      return this.state.valorSelectOrdem === 'crescente' ? a.value - b.value : b.value - a.value 
+      })
+    
+    
+    // const produtoOrdenado = () => {
+    //   this.setState({jobs: produtoOrdenaNDO})
+    // }
 
-      <CartaoTrabalho 
-      key={job.id}
-      cartaoFoto={img}
-      cartaoNome={job.title}
-      cartaoPreco={job.value}
-      cartaoDescricao={job.description}
-      cartaoMetodo={job.paymentMethods} 
-      cartaoPrazo={job.dueDate}
-    />
 
-      //  <Button
-          // texto='DELETAR'
-          // funcao={() => this.onClickDelete(job)}
-      // /> 
-      // </div>
-    )) 
 
+    // JSX (tal como o map)
+    const jobsToScreen = produtoOrdenado.map((job) => (
+
+        <CartaoTrabalho 
+        key={job.id}
+        cartaoFoto={img}
+        cartaoNome={job.title}
+        cartaoPreco={job.value}
+        cartaoDescricao={job.description}
+        cartaoMetodo={job.paymentMethods} 
+        cartaoPrazo={job.dueDate}
+      />
+  
+      )) 
+        
         
     return (
       <div>
@@ -75,9 +105,16 @@ export default class ExibirTrabalhos extends React.Component {
           <div className='exibir-inputs'>
 
             <input type='text' size={90} placeholder='Qual serviço você procura? Ex.: Assistência Técnica, Consultoria, Web Design, Reformas, Serviços Domésticos e Aulas Particulares' />
-            <select>
-              <option>Ordenar</option>
-            </select>
+            <p>Ordenar por:</p>
+            <select 
+            value={this.state.valorSelectOrdem}
+            onChange={this.onChangeSelectOrdem}
+            >
+              <option value='crescente'>Preço Menor</option>
+              <option value='decrescente'>Preço Maior</option>
+            </select> 
+
+
             <select>
               <option>Filtrar</option>
             </select>
